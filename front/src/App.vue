f098ad6fd0299b507f58e1fe360009e9e325f36d
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-05 15:00:27 -0700

    Move basic code into a src/ directory.

---
+++ front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style type="text/scss">
+.hello-world {
+  color: red;
+}
+</style>
