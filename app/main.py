a6a4100ea84717a7001797477f3ba67e288496bd
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-11 12:51:14 -0700

    Fix app.context() bug, fix tables don't exist bug.

--- app/main.py
+++ app/main.py
@@ -18,28 +18,30 @@ flask_db.init_app(app)
 db = flask_db
 
 
-
 def run_game():
     # Initialize
-    frame = 1
-    game_world = World()
-    game_world.save()
-    player1 = Colony(game_world.id)
-    player1.save()
-    player1_nest = Nest(player1.id, 25, 25)
-    player1_nest.save()
-    player1_ant = Ant(player1.id, player1_nest.id, 20, 20)
-    player1_ant.save()
-    while True:
-        print("Frame {frame}".format(frame=frame))
-        print(player1_nest)
-        print("\n\n\n")
-        for ant in player1.ants:
-            ant.move()
-        frame += 1
-        if randint(1, 100) > 100:
-            player1.birth_ant()
-        sleep(2)
+    with app.app_context():
+        db.create_all()
+
+        frame = 1
+        game_world = World()
+        game_world.save()
+        player1 = Colony(game_world.id)
+        player1.save()
+        player1_nest = Nest(player1.id, 25, 25)
+        player1_nest.save()
+        player1_ant = Ant(player1.id, player1_nest.id, 20, 20)
+        player1_ant.save()
+        while True:
+            print("Frame {frame}".format(frame=frame))
+            print(player1_nest)
+            print("\n\n\n")
+            for ant in player1.ants:
+                ant.move()
+            frame += 1
+            if randint(1, 100) > 100:
+                player1.birth_ant()
+            sleep(2)
 
 
 run_game()
