97196a3807ab2c773c5ba5c75b94d57efc3b3a8b
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-11 12:52:42 -0700

    Add naming conventions to db to allow dropping tables.

--- app/models/templates.py
+++ app/models/templates.py
@@ -5,6 +5,17 @@ from sqlalchemy.exc import DatabaseError
 from ..extensions import flask_db as db
 
 
+convention = {
+  "ix": 'ix_%(column_0_label)s',
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(column_0_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
+
+db.Model.metadata.naming_convention = convention
+
+
 class Template(db.Model):
     __abstract__ = True
 
