b1f2fe29bb1d329b54a563bd07293d28cff132a2
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-12 15:48:46 -0700

    Build basic config from tutorial.

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,57 +1,10 @@
-// webpack.config.js
-// This library allows us to combine paths easily
 const path = require('path');
 
-const { VueLoaderPlugin } = require('vue-loader');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const env = process.env.NODE_ENV;
-
-const config = {
-  entry: path.resolve(__dirname, 'src', 'main.js'),
-  mode: env,
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // default entry point
   output: {
-    publicPath: '/',
-  },
-  optimization: {
-    splitChunks: {
-      // Must be specified for HtmlWebpackPlugin to work correctly.
-      // See: https://github.com/jantimon/html-webpack-plugin/issues/882
-      chunks: 'all',
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [path.join(__dirname, 'src')],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      filename: path.join(__dirname, 'dist', 'index.html'),
-      template: path.join(__dirname, 'static', 'index.html'),
-      inject: true,
-    }),
-  ],
-};
-
-
-module.exports = config;
+    path: path.join(__dirname, 'dist'), // default path
+    filename: './main.js', // default filename
+  }
+}
