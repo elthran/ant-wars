7a0d226977b4050add972a72f8c4f4d31967eddd
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-12 21:06:38 -0700

    Basic vue app.

--- front/src/index.js
+++ front/src/index.js
@@ -1,2 +1,15 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
+
+// import VueKonva from 'vue-konva';
+
+// Vue.use(VueKonva);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  render (h) {
+    return h('app')
+  },
+});
