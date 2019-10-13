a54ce38061a5f386f7bea93dc4c67a0f40c04d8f
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-13 13:49:37 -0700

    Build basic display canvas demo.

---
+++ front/src/components/World.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  v-stage#world(:config="configKonva")
+      v-layer
+        v-circle(:config="configCircle")
+</template>
+
+<script>
+export default {
+  name: 'World',
+  data () {
+    return {
+      configKonva: {
+        width: 200,
+        height: 200,
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: "red",
+        stroke: "black",
+        strokeWidth: 4,
+      },
+    }
+  },
+}
+</script>
