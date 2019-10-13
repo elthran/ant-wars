12795b08e5e3ea1276a033e40de61d815e2ec0db
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-13 13:19:50 -0700

    Refactor webpack config into several modules.
    Move .babelrc into webpack config for consistency.

---
+++ front/config/babel.config.js
@@ -0,0 +1,22 @@
+// html.config.js
+
+
+module.exports = {
+  module: {
+    rules: [
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-syntax-dynamic-import'],
+          },
+        },
+      },
+    ],
+  },
+}
