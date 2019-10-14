a67fcdfaf9d8e8924b128f46c691e3d5391e5f63
Marlen Brunner <klondikemarlen@gmail.com>
2019-10-14 12:26:17 -0700

    Add static asset copying to dist.

--- front/config/copy.config.js
+++ front/config/copy.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   plugins: [
     new CopyPlugin([
-      { from: 'assets', to: 'assets' },
+      { from: 'assets/**', to: '' },
     ]),
   ],
 }
