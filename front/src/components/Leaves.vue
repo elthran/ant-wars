423b76cd460f37d5016016c2317e19880a0ac8b3
Marlen Brunner <klondikemarlen@gmail.com>
2020-01-01 13:12:19 -0800

    Debug bad merge.

--- front/src/components/Leaves.vue
+++ front/src/components/Leaves.vue
@@ -1,21 +1,4 @@
 <template lang="pug">
-  div
-    h1 Hello World
-</template>
-
-<script>
-export default {
-  name: 'Leaves',
-  components: {
-  },
-  props: {
-
-  },
-  data () {
-    return {}
-  },
-  computed: {},
-  methods: {},
   v-layer
     v-group(
       v-for="leaf in leaves"
