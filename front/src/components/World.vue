f5139a27b4a74b3dc7d2afc0926d67896feaa436
Marlen Brunner <klondikemarlen@gmail.com>
2020-01-02 18:05:04 -0800

    :recycle: Drop old code.

--- front/src/components/World.vue
+++ front/src/components/World.vue
@@ -74,55 +74,6 @@ export default {
   beforeDestroy () {
     // clearInterval(this.interval);
   },
-  methods: {
-    randomBetween(min = 3, max = 20) {
-      return Math.floor((Math.random() * (max-min)) + min) // 1-100
-    },
-    roundToMultiple(value, interval) {
-      const diffInterval = value % interval
-      if (diffInterval < interval / 2.0) {
-        return value - diffInterval
-      } else {
-        return value + (interval - diffInterval)
-      }
-    },
-    randomAnts() {
-      let ants = []
-      let numOfAnts = this.randomBetween(2, 20)
-
-      do {
-        ants.push({
-          id: numOfAnts,
-          x: this.randomXCoord(),
-          y: this.randomYCoord(),
-        })
-        numOfAnts--
-      } while (numOfAnts > 0)
-
-      return ants
-    },
-    randomLeaves() {
-      let leaves = []
-      let numOfLeaves = this.randomBetween(2, 20)
-
-      do {
-        leaves.push({
-          id: numOfLeaves,
-          x: this.randomXCoord(),
-          y: this.randomYCoord(),
-        })
-        numOfLeaves--
-      } while (numOfLeaves > 0)
-
-      return leaves
-    },
-    randomXCoord() {
-      return this.randomBetween(1, this.xGridUnits) * 25
-    },
-    randomYCoord() {
-      return this.randomBetween(1, this.yGridUnits) * 25
-    },
-  }
 }
 </script>
 
